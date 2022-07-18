ced95ee50f5f35abe520869c1cc2b9fc1a438239
Egor Vlasov <egorcrash05@gmail.com>
2022-07-18 10:54:22 +0200

    trigger deploy again

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # Returns a random token.
+  # Returns a random token
   def self.new_token
     SecureRandom.urlsafe_base64
   end
